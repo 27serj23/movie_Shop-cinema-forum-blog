01115a451170d525a6cedb790bc8d0589fe9b5db
27serj23 <154461692+27serj23@users.noreply.github.com>
2024-07-09 23:48:43 +0300

    Add files via upload

---
+++ online_cinema/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Feedback(models.Model):
+    text = models.CharField(max_length=400)
+    created_date = models.DateTimeField(auto_now_add=True)
+    film_name = models.CharField(max_length=300, null=True)
+    update = models.BooleanField(default=False)
+    updated_date = models.DateTimeField(auto_now=True)
